86f02f155548b52a6cb42159713f5825e26d976b
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 11:37:52 -0400

    added location addresss

--- src/index.js
+++ src/index.js
@@ -19,14 +19,15 @@ let ul = document.createElement("ul")
 let weekdayHours = document.createElement("li")
 let satHours = document.createElement("li")
 let sunHours = document.createElement("li")
-
 weekdayHours.textContent = "Mon - Fri: 8am - 8pm"
 satHours.textContent = "Sat: 8am - 10pm"
 sunHours.textContent = "Sun: 9am - 7pm"
-
 ul.appendChild(weekdayHours)
 ul.appendChild(satHours)
 ul.appendChild(sunHours)
-
 hours.appendChild(ul)
 content.appendChild(hours)
+
+location.setAttribute("id", "location")
+location.textContent = "1600 Pennsylvania Avenue NW, Washington, DC 20500"
+content.appendChild(location)
