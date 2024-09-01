9436859f4d503915f86c4e4d55905c7554bf924a
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 11:39:39 -0400

    put it inside a funtion

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import './styles.css'
 
-let content = document.getElementById("content")
-let title = document.createElement("h1")
-let summary = document.createElement("div")
-let hours = document.createElement("div")
-let location = document.createElement("div")
+function initial_page_load () {
+    let content = document.getElementById("content")
+    let title = document.createElement("h1")
+    let summary = document.createElement("div")
+    let hours = document.createElement("div")
+    let location = document.createElement("div")
 
-title.setAttribute("id", "title")
-title.textContent = "Title"
-content.appendChild(title)
+    title.setAttribute("id", "title")
+    title.textContent = "Title"
+    content.appendChild(title)
 
-summary.setAttribute("id", "summary")
-summary.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, dignissimos ratione! Cumque voluptate in, assumenda, accusamus minima recusandae fugit doloremque tempora nostrum odio ipsam sint praesentium quis dolores, quibusdam ratione."
-content.appendChild(summary)
+    summary.setAttribute("id", "summary")
+    summary.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, dignissimos ratione! Cumque voluptate in, assumenda, accusamus minima recusandae fugit doloremque tempora nostrum odio ipsam sint praesentium quis dolores, quibusdam ratione."
+    content.appendChild(summary)
 
-hours.setAttribute("id", "hours")
-let ul = document.createElement("ul")
-let weekdayHours = document.createElement("li")
-let satHours = document.createElement("li")
-let sunHours = document.createElement("li")
-weekdayHours.textContent = "Mon - Fri: 8am - 8pm"
-satHours.textContent = "Sat: 8am - 10pm"
-sunHours.textContent = "Sun: 9am - 7pm"
-ul.appendChild(weekdayHours)
-ul.appendChild(satHours)
-ul.appendChild(sunHours)
-hours.appendChild(ul)
-content.appendChild(hours)
+    hours.setAttribute("id", "hours")
+    let ul = document.createElement("ul")
+    let weekdayHours = document.createElement("li")
+    let satHours = document.createElement("li")
+    let sunHours = document.createElement("li")
+    weekdayHours.textContent = "Mon - Fri: 8am - 8pm"
+    satHours.textContent = "Sat: 8am - 10pm"
+    sunHours.textContent = "Sun: 9am - 7pm"
+    ul.appendChild(weekdayHours)
+    ul.appendChild(satHours)
+    ul.appendChild(sunHours)
+    hours.appendChild(ul)
+    content.appendChild(hours)
 
-location.setAttribute("id", "location")
-location.textContent = "1600 Pennsylvania Avenue NW, Washington, DC 20500"
-content.appendChild(location)
+    location.setAttribute("id", "location")
+    location.textContent = "1600 Pennsylvania Avenue NW, Washington, DC 20500"
+    content.appendChild(location)
+}
+
+initial_page_load()
