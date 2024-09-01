03981174267ee70998da3fa50620f17cd80787e1
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 12:18:58 -0400

    added about and menu js for content creation specific to each page

---
+++ src/about.js
@@ -0,0 +1,4 @@
+function about_page_load() {
+    content = document.getElementById("content")
+    
+}
---
+++ src/menu.js
@@ -0,0 +1,4 @@
+function menu_page_load() {
+    content = document.getElementById("content")
+    
+}
