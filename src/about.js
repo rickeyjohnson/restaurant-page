ccde63e143cadc839e14f1b6ef5e66cd582bd546
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 13:50:07 -0400

    added about html load up

--- src/about.js
+++ src/about.js
@@ -1,3 +1,7 @@
-const about_page_load = () => {
+export const about_page_load = () => {
     let content = document.getElementById("content")
+    
+    let about = document.createElement("div")
+    about.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo minus impedit animi voluptate sit quisquam magni magnam dignissimos voluptates quis unde cumque, deleniti adipisci sed cum eaque alias sequi odit?"
+    content.appendChild(about)
 }
