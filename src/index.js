fd75987d663c304bc13ce674425f59d1031b74e3
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 11:26:50 -0400

    added title and summary through js and removed it from html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css'
 
-alert('HELLO')
+let content = document.getElementById("content")
+let title = document.createElement("h1")
+let summary = document.createElement("div")
+let hours = document.createElement("div")
+let location = document.createElement("div")
+
+title.setAttribute("id", "title")
+title.textContent = "Title"
+content.appendChild(title)
+
+summary.setAttribute("id", "summary")
+summary.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, dignissimos ratione! Cumque voluptate in, assumenda, accusamus minima recusandae fugit doloremque tempora nostrum odio ipsam sint praesentium quis dolores, quibusdam ratione."
+content.appendChild(summary)
