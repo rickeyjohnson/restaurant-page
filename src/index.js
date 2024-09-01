00d7354fbb311d78a746305549cc77c792a478bd
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-01 11:36:12 -0400

    added "store" hours to webpage

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@ content.appendChild(title)
 summary.setAttribute("id", "summary")
 summary.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, dignissimos ratione! Cumque voluptate in, assumenda, accusamus minima recusandae fugit doloremque tempora nostrum odio ipsam sint praesentium quis dolores, quibusdam ratione."
 content.appendChild(summary)
+
+hours.setAttribute("id", "hours")
+let ul = document.createElement("ul")
+let weekdayHours = document.createElement("li")
+let satHours = document.createElement("li")
+let sunHours = document.createElement("li")
+
+weekdayHours.textContent = "Mon - Fri: 8am - 8pm"
+satHours.textContent = "Sat: 8am - 10pm"
+sunHours.textContent = "Sun: 9am - 7pm"
+
+ul.appendChild(weekdayHours)
+ul.appendChild(satHours)
+ul.appendChild(sunHours)
+
+hours.appendChild(ul)
+content.appendChild(hours)
